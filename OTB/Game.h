9c00019c9c04d14f8e4367ca9cc8d55192823a06
mail.bertrand.faure@gmail.com <mail.bertrand.faure@gmail.com@fd657111-4e41-582e-33ec-bbed8e1d85f6>
2012-07-12 20:01:49 +0000

    Ajout de fichiers

---
+++ OTB/Game.h
@@ -0,0 +1,16 @@
+
+#ifndef __GAME_H__
+#define __GAME_H__
+
+#pragma once
+
+class Game
+{
+public:
+	Game();
+protected:
+private:
+};
+
+
+#endif	//__GAME_H__
---
+++ OTB/Player.h
@@ -0,0 +1,17 @@
+
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#pragma once
+
+class Player
+{
+public:
+	Player();
+
+protected:
+private:
+};
+
+
+#endif	//__PLAYER_H__
