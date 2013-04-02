58f871d592dcbe366f349d54c1d15f9b06e3d26d
mail.bertrand.faure@gmail.com <mail.bertrand.faure@gmail.com@fd657111-4e41-582e-33ec-bbed8e1d85f6>
2013-04-02 17:34:35 +0000

    menu suite

--- OTB/Menus/Menu.cpp
+++ OTB/Menus/Menu.cpp
@@ -45,49 +45,24 @@ void Menu::Kill()
 	_pOTB =NULL;
 }
 
+
 // ****************************************************************************************
-//	StartMenuMain
+//	ClearMenu
 // ****************************************************************************************
-void Menu::StartMenuMain()
+void Menu::ClearMenu()
 {
+	_pGUI->Leave();
 	_pGUI->Clear();
-
-	Float32 rPosX =0.0f;
-	Float32 rPosY =_pOTB->GetGame().GetLevel().GetSize().y*0.7f;
-	Float32 rDelay=0.0f;
-
-	_pGUI->AddCtrl(new hgeGUIMenuItem(MII_MAIN_PLAY,	this, rPosX, rPosY, "Play")	);		rPosY -=0.4f;
-	_pGUI->AddCtrl(new hgeGUIMenuItem(MII_MAIN_OPTIONS, this, rPosX, rPosY, "Options"));	rPosY -=0.4f;
-	_pGUI->AddCtrl(new hgeGUIMenuItem(MII_MAIN_HELP,	this, rPosX, rPosY, "Help")	);		rPosY -=0.4f;
-	_pGUI->AddCtrl(new hgeGUIMenuItem(MII_MAIN_CREDITS, this, rPosX, rPosY, "Credits"));	rPosY -=0.4f;
-	_pGUI->AddCtrl(new hgeGUIMenuItem(MII_MAIN_EXIT,	this, rPosX, rPosY, "Exit")	);		rPosY -=0.4f;
-
-	_pGUI->SetFocus(MII_MAIN_PLAY);
-	_pGUI->Enter();
-
-	_nMenuCurrent =MS_MAIN;
+	_rCurrentY =_pOTB->GetGame().GetLevel().GetSize().y*0.7f;
 }
 
 // ****************************************************************************************
-//	StartMenuOptions
+//	AddMenuItem
 // ****************************************************************************************
-void Menu::StartMenuOptions()
+void Menu::AddMenuItem(const MenuItemId ItemId, const TString& sLabel)
 {
-	_pGUI->Clear();
-
-	Float32 rPosX =0.0f;
-	Float32 rPosY =_pOTB->GetGame().GetLevel().GetSize().y*0.7f;
-	Float32 rDelay=0.0f;
-
-	_pGUI->AddCtrl(new hgeGUIMenuItem(MII_OPTIONS_VIDEO,	this, rPosX, rPosY, "Video"));	rPosY -=0.4f;
-	_pGUI->AddCtrl(new hgeGUIMenuItem(MII_OPTIONS_AUDIO,	this, rPosX, rPosY, "Audio"));	rPosY -=0.4f;
-	_pGUI->AddCtrl(new hgeGUIMenuItem(MII_OPTIONS_INPUT,	this, rPosX, rPosY, "Input"));	rPosY -=0.4f;
-	_pGUI->AddCtrl(new hgeGUIMenuItem(MII_OPTIONS_BACK,		this, rPosX, rPosY, "Back")	);	rPosY -=0.4f;
-
-	_pGUI->SetFocus(MII_OPTIONS_VIDEO);
-	_pGUI->Enter();
-
-	_nMenuCurrent =MS_OPTIONS;
+	_pGUI->AddCtrl(new hgeGUIMenuItem(ItemId, this, 0.0f, _rCurrentY, sLabel.GetCharconst()) );
+	_rCurrentY -=0.4f;
 }
 
 // ****************************************************************************************
@@ -103,6 +78,7 @@ void Menu::Update(Float32 dt)
 
 		switch(id)
 		{
+			// ------------- Menu Main ------------- 
 		case MII_MAIN_PLAY:
 			_pOTB->GetGame().Kill();
 			_pOTB->GetGame().InitByXml(_pOTB, &_pOTB->GetXmlTree());
@@ -113,33 +89,192 @@ void Menu::Update(Float32 dt)
 			StartMenuOptions();
 			break;
 
-		case MII_OPTIONS_BACK:
-			StartMenuMain();
-			break;
-
 		case MII_MAIN_HELP:
-			{
-				hge->Gfx_SetDisplayMode(320, 200, 32);
-				_pGUI->SetFocus(1);
-				_pGUI->Enter();
-			}
-			break;
-		case MII_MAIN_CREDITS:
-			hge->Gfx_SetDisplayMode(32, 17, 32);
+			StartMenuHelp();
 			break;
 
-//		case 4:
-//			_pGUI->SetFocus(1);
-//			_pGUI->Enter();
-//			break;
+		case MII_MAIN_CREDITS:
+			StartMenuCredits();
+			break;
 
 		case MII_MAIN_EXIT:
 			_pOTB->ExitApp();
 			break;
 
+			// ------------- Menu Options ------------- 
+		case MII_OPTIONS_VIDEO:
+			StartMenuVideo();
+			break;
+		case MII_OPTIONS_AUDIO:
+			StartMenuAudio();
+			break;
+		case MII_OPTIONS_INPUT:
+			StartMenuInput();
+			break;
+		case MII_OPTIONS_BACK:
+			StartMenuMain();
+			break;
+
+			// ------------- Menu Video ------------- 
+		case MII_OPTVIDEO_RESOLUTION:
+			break;
+
+		case MII_OPTVIDEO_WINDOWED:
+			break;
+
+		case MII_OPTVIDEO_BACK:
+			StartMenuOptions();
+			break;
+
+			// ------------- Menu Audio ------------- 
+		case MII_OPTAUDIO_BACK:
+			StartMenuOptions();
+			break;
+
+			// ------------- Menu Input ------------- 
+		case MII_OPTINPUT_BACK:
+			StartMenuOptions();
+			break;
+
+			// ------------- Menu Help ------------- 
+		case MII_HELP_BACK:
+			StartMenuMain();
+			break;
+
+			// ------------- Menu Credits ------------- 
+		case MII_CREDITS_BACK:
+			StartMenuMain();
+			break;
+
 		default:;
 		}
 	}
+}
+
+// hge->Gfx_SetDisplayMode(1280, 720, 32);
+// _pGUI->SetFocus(1);
+// _pGUI->Enter();
+// 
+
+
+// hge->Gfx_SetDisplayMode(320, 200, 32);
+// _pGUI->SetFocus(1);
+// _pGUI->Enter();
+
+//hge->Gfx_SetDisplayMode(32, 17, 32);
+
+// ****************************************************************************************
+//	StartMenuMain
+// ****************************************************************************************
+void Menu::StartMenuMain()
+{
+	ClearMenu();
+
+	AddMenuItem( MII_MAIN_PLAY,		"Play"		);
+	AddMenuItem( MII_MAIN_OPTIONS,	"Options"	);
+	AddMenuItem( MII_MAIN_HELP,		"Help"		);
+	AddMenuItem( MII_MAIN_CREDITS,	"Credits"	);
+	AddMenuItem( MII_MAIN_EXIT,		"Exit"		);
+
+	_pGUI->SetFocus(MII_MAIN_PLAY);
+	_pGUI->Enter();
+
+	_nMenuCurrent =MS_MAIN;
+}
+
+// ****************************************************************************************
+//	StartMenuOptions
+// ****************************************************************************************
+void Menu::StartMenuOptions()
+{
+	ClearMenu();
+
+	AddMenuItem( MII_OPTIONS_VIDEO,	"Video"		);
+	AddMenuItem( MII_OPTIONS_AUDIO,	"Audio"		);
+	AddMenuItem( MII_OPTIONS_INPUT,	"Input"		);
+	AddMenuItem( MII_OPTIONS_BACK,	"Back"		);
+
+	_pGUI->SetFocus(MII_OPTIONS_VIDEO);
+	_pGUI->Enter();
+
+	_nMenuCurrent =MS_OPTIONS;
+}
+
+// ****************************************************************************************
+//	StartMenuVideo
+// ****************************************************************************************
+void Menu::StartMenuVideo()
+{
+	ClearMenu();
+
+	AddMenuItem( MII_OPTVIDEO_RESOLUTION,	"Resolution"	);
+	AddMenuItem( MII_OPTVIDEO_WINDOWED,		"Windowed"		);
+	AddMenuItem( MII_OPTVIDEO_BACK,			"Back"			);
+
+	_pGUI->SetFocus(MII_OPTVIDEO_RESOLUTION);
+	_pGUI->Enter();
+
+	_nMenuCurrent =MS_VIDEO;
+}
+
+// ****************************************************************************************
+//	StartMenuAudio
+// ****************************************************************************************
+void Menu::StartMenuAudio()
+{
+	ClearMenu();
+
+	AddMenuItem( MII_OPTAUDIO_BACK,			"Back"			);
+
+	_pGUI->SetFocus(MII_OPTAUDIO_BACK);
+	_pGUI->Enter();
+
+	_nMenuCurrent =MS_AUDIO;
+}
+
+// ****************************************************************************************
+//	StartMenuInput
+// ****************************************************************************************
+void Menu::StartMenuInput()
+{
+	ClearMenu();
+
+	AddMenuItem( MII_OPTINPUT_BACK,			"Back"			);
+
+	_pGUI->SetFocus(MII_OPTINPUT_BACK);
+	_pGUI->Enter();
+
+	_nMenuCurrent =MS_INPUT;
+}
+
+// ****************************************************************************************
+//	StartMenuHelp
+// ****************************************************************************************
+void Menu::StartMenuHelp()
+{
+	ClearMenu();
+
+	AddMenuItem( MII_HELP_BACK,			"Back"			);
+
+	_pGUI->SetFocus(MII_HELP_BACK);
+	_pGUI->Enter();
+
+	_nMenuCurrent =MS_HELP;
+}
+
+// ****************************************************************************************
+//	StartMenuCredits
+// ****************************************************************************************
+void Menu::StartMenuCredits()
+{
+	ClearMenu();
+
+	AddMenuItem( MII_CREDITS_BACK,			"Back"			);
+
+	_pGUI->SetFocus(MII_CREDITS_BACK);
+	_pGUI->Enter();
+
+	_nMenuCurrent =MS_CREDITS;
 }
 
 // ****************************************************************************************
@@ -153,7 +288,3 @@ void Menu::Render()
 	}
 }
 
-// hge->Gfx_SetDisplayMode(1280, 720, 32);
-// _pGUI->SetFocus(1);
-// _pGUI->Enter();
-// 
